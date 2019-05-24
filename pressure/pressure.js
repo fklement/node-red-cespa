db7f0eab64927f07c095c937fea3ab65f9fd47c2
Martin <martin.altmann97@web.de>
2019-05-24 13:18:33 +0200

    Implementation of acceleration, pressure and charge node

---
+++ pressure/pressure.js
@@ -0,0 +1,43 @@
+module.exports = function (RED) {
+    function PressureNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.on('input', function (msg) {
+            const fs = require('fs'),
+                path = require('path'),
+                certFile = path.resolve(__dirname, '../ssl/RESTTEST_cert.pem'),
+                keyFile = path.resolve(__dirname, '../ssl/RESTTEST_key.pem'),
+                request = require('request');
+
+            const query = {
+                "db": "tires",
+                "schema": "hackaton",
+                "table": "ruuvidata",
+                 //"where": {
+                 //    "DID": {
+                 //        "=": "RESTTEST"
+                 //    }
+                 //}
+            }
+
+            var buff = new Buffer(JSON.stringify(query)).toString("base64");
+
+            console.log(query.toString("base64"))
+
+            const options = {
+                url: "https://ctpwyd.conti.de:443/data?q=" + buff,
+                cert: fs.readFileSync(certFile),
+                key: fs.readFileSync(keyFile)
+            };
+
+            request.get(options, function (error, response, body) {
+                msg.payload = body;
+                node.send(msg);
+            });
+
+
+
+        });
+    }
+    RED.nodes.registerType("pressure", PressureNode);
+}
