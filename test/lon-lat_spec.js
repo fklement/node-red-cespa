0ac5a7e9d3b556ee801df48ed1708a78dfc616d4
Felix Klement <mail@felix-klement.de>
2019-05-25 02:40:13 +0200

    Remove lower case test

--- test/lon-lat_spec.js
+++ test/lon-lat_spec.js
@@ -29,31 +29,4 @@ describe('lon-lat Node', function () {
       done();
     });
   });
-
-  it('should make payload lower case', function (done) {
-    var flow = [{
-        id: "n1",
-        type: "lower-case",
-        name: "lower-case",
-        wires: [
-          ["n2"]
-        ]
-      },
-      {
-        id: "n2",
-        type: "helper"
-      }
-    ];
-    helper.load(lowerNode, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      n2.on("input", function (msg) {
-        msg.should.have.property('payload', 'uppercase');
-        done();
-      });
-      n1.receive({
-        payload: "UpperCase"
-      });
-    });
-  });
 });
